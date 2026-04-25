d630581bd3a63f5401779deb984e15817e7a8ca9
RealOshina <130570122+RealOshina@users.noreply.github.com>
2026-04-25 13:58:57 +0700

    v0.2.1 - Complete the circuit file

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from electricalsim import Loader, Circuit
+from electricalsim import VoltageSource, Resistor
 
 def main():
     data = Loader.open_file("data/2026-19-4_circuit_1.json")
-    circuit = Circuit(data)
+    try:
+        circuit = Circuit(data)
+        print(circuit)
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
     main()
--- src/electricalsim/core/circuit.py
+++ src/electricalsim/core/circuit.py
@@ -1,20 +1,29 @@
-from .loader import Loader
-from ..components import capacitor
-from ..components import current_source
-from ..components import diode
-from ..components import inductor
-from ..components import resistor
-from ..components import transistor
-from ..components import voltage_source
+from ..components.capacitor import Capacitor
+from ..components.current_source import CurrentSource
+from ..components.diode import Diode
+from ..components.inductor import Inductor
+from ..components.resistor import Resistor
+from ..components.transistor import Transistor
+from ..components.voltage_source import VoltageSource
 
 _VALID_COMPONENT = {
-    "capacitor": capacitor,
-    "current_source": current_source,
-    "diode": diode,
-    "inductor": inductor,
-    "resistor": resistor,
-    "transistor": transistor,
-    "voltage_source": voltage_source
+    "capacitor": Capacitor,
+    "current_source": CurrentSource,
+    "diode": Diode,
+    "inductor": Inductor,
+    "resistor": Resistor,
+    "transistor": Transistor,
+    "voltage_source": VoltageSource
+}
+
+_COMPONENT = {
+    "capacitor": "capacitance",
+    "current_source": "current",
+    "diode": "forward_voltage",
+    "inductor": "inductance",
+    "resistor": "resistance",
+    "transistor": "hfe",
+    "voltage_source": "voltage"
 }
 
 class Circuit:
@@ -22,22 +31,33 @@ class Circuit:
         self.data = data
         self.node = data["nodes"]
         self.components = data["components"]
-        self.valid_check()
-    def valid_check(self):
-        """
-        for component in self.components:
-            for key in _VALID_COMPONENT:
-                if component["type"] == key:
-                    print("a")
-                else:
-                    print("b")
-            print(component, key)
-        """
-        for component in self.components:
-            for key in _VALID_COMPONENT:
-                print(key)
-            if component["type"] == _VALID_COMPONENT:
-                print("a")
+        self.valid_check(self.components)
+    def valid_check(self, components):
+        self.result = []
+        for component in components:
+            component_type = component["type"]
+            if component_type in _VALID_COMPONENT:
+                #print("a", component_type)
+
+                class_ = _VALID_COMPONENT[component_type]
+                object = None
+                
+                a = {}
+
+                for i, x in component.items():
+                    if i != "type":
+                        if i == "value":
+                            i = _COMPONENT[component_type]
+                        
+                        a[i] = x
+                        object = {i: x}
+                        
+                        #print(object, i, a)
+                
+                self.result.append(a)
+                object = class_(**{i:x})
+                #print(self.result)
             else:
-                print("b")
-            print(component, component["type"])
+                raise ValueError(f"Unknown component type: {component_type}")
+    def __str__(self):
+        return str(self.result)
